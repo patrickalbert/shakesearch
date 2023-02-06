ef5bbdaac29058ac3b02903a22134ff2eb482e0f
Patrick Albert <patrick.albert222@gmail.com>
2023-02-06 20:26:00 -0300

    add loading spinner

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,20 +4,22 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
+import Spinner from 'react-bootstrap/Spinner';
 import './App.css';
 
 function App() {
   const [query, setQuery] = useState('');
   const [matchCaseToggle, setMatchCaseToggle] = useState(false);
-  const [queryResults, setQueryResults] = useState([]);
+  const [queryResults, setQueryResults] = useState(null);
   const [currentIndex, setCurrentIndex] = useState(0);
   const [readingPanelContent, setReadingPanelContent] = useState('');
+  const [loading, setLoading] = useState(false);
 
   const apiBase = process.env.REACT_APP_API;
   
   
   useEffect(() => {
-    if (queryResults.length > 0 && currentIndex > 0) {
+    if (queryResults?.length > 0 && currentIndex > 0) {
       const requestPath = `${apiBase}/reader?idx=${currentIndex}`
       const response = fetch(requestPath).then((response) => {
         response.json().then((result) => {
@@ -28,6 +30,7 @@ function App() {
   }, [currentIndex, readingPanelContent, queryResults, apiBase])
 
   const handleSubmit = (values) => {
+    setLoading(true);
     const queryValue = values.query;
     const matchCaseValue = values.matchCase;
     const wholeWordValue = values.wholeWord;
@@ -38,7 +41,8 @@ function App() {
 
     const response = fetch(requestPath).then((response) => {
       response.json().then((results) => {
-        setQueryResults(results)
+        setQueryResults(results);
+        setLoading(false);
       });
     });
 
@@ -121,33 +125,48 @@ function App() {
               </Form>
             </Formik>
         </Row>
-        <Row>
-          {queryResults.length > 0 && (
-            <strong>{`Results: ${queryResults.length}`}</strong>
-          )}
-          <Col sm={ currentIndex > 0 ? 6 : 12 }>
-            <div className="search-results-panel overflow-scroll">
-              {queryResults.length > 0 && queryResults.map((result, index) => (
-                <div key={`result-${index}`} className="search-result" onClick={() => handleResultClick(result.Index)}>
-                  <strong>Result # {index + 1}</strong><br />
-                  <div className="result-text" dangerouslySetInnerHTML={{__html: insertMarks(result.Result)}}></div>
-                  <strong><i>{result.LocationTitle}</i></strong>
-                  <hr />
+        {loading && (
+          <div>
+            Loading Results...
+            <Spinner animation="border" role="status">
+              <span className="visually-hidden">Loading...</span>
+            </Spinner>
+          </div>
+        )}
+        {!loading && (
+          <Row>
+            {queryResults?.length > 0 && (
+              <strong>{`Results: ${queryResults.length}`}</strong>
+            )}
+            <Col sm={ currentIndex > 0 ? 6 : 12 }>
+              {queryResults?.length === 0 && (
+                <div>
+                  <strong>No results found</strong>
                 </div>
-              ))}
-            </div>
-          </Col>
-          {currentIndex > 0 && (
-            <Col sm={6}>
-              <Button className="page-nav-btn" variant="light" onClick={() => fetchPreviousPage()}>Previous Page</Button>
-              <Button className="page-nav-btn" variant="light" onClick={() => fetchNextPage()}>Next Page</Button>
-              <Button className="page-nav-btn" variant="danger" onClick={() => setCurrentIndex(0)}>X</Button>
-              <div className="reading-panel overflow-scroll">
-                <div className="result-text" dangerouslySetInnerHTML={{__html: insertMarks(readingPanelContent)}}></div>
+              )}
+              <div className="search-results-panel overflow-scroll">
+                {queryResults?.length > 0 && queryResults.map((result, index) => (
+                  <div key={`result-${index}`} className="search-result" onClick={() => handleResultClick(result.Index)}>
+                    <strong>Result # {index + 1}</strong><br />
+                    <div className="result-text" dangerouslySetInnerHTML={{__html: insertMarks(result.Result)}}></div>
+                    <strong><i>{result.LocationTitle}</i></strong>
+                    <hr />
+                  </div>
+                ))}
               </div>
             </Col>
-          )}
-        </Row>
+            {currentIndex > 0 && (
+              <Col sm={6}>
+                <Button className="page-nav-btn" variant="light" onClick={() => fetchPreviousPage()}>Previous Page</Button>
+                <Button className="page-nav-btn" variant="light" onClick={() => fetchNextPage()}>Next Page</Button>
+                <Button className="page-nav-btn" variant="danger" onClick={() => setCurrentIndex(0)}>X</Button>
+                <div className="reading-panel overflow-scroll">
+                  <div className="result-text" dangerouslySetInnerHTML={{__html: insertMarks(readingPanelContent)}}></div>
+                </div>
+              </Col>
+            )}
+          </Row>
+        )}
       </Container>
     </div>
   );
